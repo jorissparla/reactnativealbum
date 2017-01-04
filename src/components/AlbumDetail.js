d872c8b5ed7a9f7ccab512e684d6d05981537c6b
jorissparla <joris.sparla@gmail.com>
2017-01-04 14:30:56 +0100

    complete

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Text, View , Image} from 'react-native';
+import { Text, View , Image, Linking } from 'react-native';
 import Card from './Card'
 import CardSection from './CardSection'
 import Button from './Button'
 
 const AlbumDetail = ({album}) => {
-  const {title, artist, thumbnail_image, image} = album;
+  const {title, artist, thumbnail_image, image, url} = album;
   const { headerContentStyle, 
           imageStyle, 
           thumbnailContainerStyle, 
@@ -34,7 +34,9 @@ const AlbumDetail = ({album}) => {
       </CardSection>
 
       <CardSection>
-        <Button text='Hallo'/>
+        <Button  onPress={()=> Linking.openURL(url)}>
+          Buy this Album..🎵🎵
+        </Button>
       </CardSection>
 
       
